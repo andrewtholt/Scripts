75efc54d5412437ceaacdc1a334d814332110495
andrewtholt <andrew_tholt@me.com>
2015-03-17 09:39:42 +0000

    Updat svn too

---
+++ update.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -x
+
+RC=$HOME/Source
+GITS=`find $SRC -name .git`
+
+echo "Update from GIT"
+for PLACE in $GITS; do
+        echo `dirname $PLACE`
+        HERE=`pwd`
+        cd `dirname $PLACE`
+        git pull
+        cd $HERE
+done
+echo "================================"
+
+SVNS=`find $SRC -name .svn`
+echo "Update from SVN"
+
+for PLACE in $SVNS; do
+        echo `dirname $PLACE`
+        HERE=`pwd`
+        cd `dirname $PLACE`
+        svn update
+        cd $HERE
+done
+echo "================================"
