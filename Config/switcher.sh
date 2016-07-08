1f99be81f2174b3d73a406eb2c8c26abd495be00
andrewtholt <andrewtholt60@gmail.com>
2016-07-08 19:07:19 +0100

    Select version of gcc & g++ t use.

---
+++ Config/switcher.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+if [ $# -eq 0 ]; then
+    echo 
+    echo "Enter version number to switch to, choices are:"
+    echo
+    update-alternatives --query gcc | grep Alter |cut -f2 -d":"
+    echo
+    exit 0
+fi
+
+CNT=$(update-alternatives --query gcc | grep Alter | grep "\-$1$" | wc -l)
+
+if [ $CNT -eq 0 ]; then
+    echo "gcc Version $1 unknown"
+else
+    TOOL_PATH=$(which gcc-$1)
+    update-alternatives --set gcc $TOOL_PATH
+fi
+
+CNT=$(update-alternatives --query g++ | grep Alter | grep "\-$1" | wc -l)
+if [ $CNT -eq 0 ]; then
+    echo "g++ Version $1 unknown."
+else
+    TOOL_PATH=$(which g++-$1)
+    update-alternatives --set g++ $TOOL_PATH
+fi
+
+update-alternatives --query gcc
+echo "==============================="
+update-alternatives --query g++
